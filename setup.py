10431d3a7608cf2da396e9eeb4404a5d7c344007
Bas Nijholt <basnijholt@gmail.com>
2019-11-29 12:16:53 +0100

    bumpverion fixes 🛠

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ install_requires = ["Click", "crossrefapi", "diskcache", "requests", "pyyaml", "
 setup(
     name="yaml2bib",
     python_requires=">=3.6",
+    version="0.1.0.dev0",
     classifiers=[
         "Intended Audience :: Science/Research",
         "Operating System :: OS Independent",
