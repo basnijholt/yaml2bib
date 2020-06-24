4fd45a505f6f8cb1db445284ff370eb1e48ce0c5
Bas Nijholt <basnijholt@gmail.com>
2020-06-24 12:28:07 +0200

    use fake email address

--- tests/test_yaml2bib.py
+++ tests/test_yaml2bib.py
@@ -1,5 +1,7 @@
+import yaml2bib
+
+
 def test_import():
-    import yaml2bib
 
     yaml2bib.yaml2bib(
         bib_fname="tests/example.bib",
@@ -8,5 +10,5 @@ def test_import():
         static_bib="tests/not_on_crossref.bib",
         doi2bib_database="tests/yaml2bib-doi2bib.db",
         crossref_database="tests/yaml2bib-crossref.db",
-        email="bas@nijho.lt",
+        email="basnijholt-yaml2bib@github.com",  # fake email
     )
