7000f0855bfb7a6c2034022be76a414951fb8ee0
Bas Nijholt <basnijholt@gmail.com>
2020-06-24 12:22:55 +0200

    fix tests and cache dbs

--- tests/test_yaml2bib.py
+++ tests/test_yaml2bib.py
@@ -1,4 +1,12 @@
 def test_import():
     import yaml2bib
 
-    yaml2bib
+    yaml2bib.yaml2bib(
+        bib_fname="tests/example.bib",
+        dois_yaml="tests/example.yaml",
+        replacements_yaml="tests/replacements.yaml",
+        static_bib="tests/not_on_crossref.bib",
+        doi2bib_database="tests/yaml2bib-doi2bib.db",
+        crossref_database="tests/yaml2bib-crossref.db",
+        email="bas@nijho.lt",
+    )
