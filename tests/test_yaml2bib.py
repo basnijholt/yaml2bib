130d4a720b28a905e57cc1f92182a81d2ab36b3c
Bas Nijholt <basnijholt@gmail.com>
2020-06-24 15:08:09 +0200

    move files to examples

--- tests/test_yaml2bib.py
+++ tests/test_yaml2bib.py
@@ -4,11 +4,11 @@ import yaml2bib
 def test_import():
 
     yaml2bib.yaml2bib(
-        bib_fname="tests/example.bib",
-        dois_yaml="tests/example.yaml",
-        replacements_yaml="tests/replacements.yaml",
-        static_bib="tests/not_on_crossref.bib",
-        doi2bib_database="tests/yaml2bib-doi2bib.db",
-        crossref_database="tests/yaml2bib-crossref.db",
+        bib_fname="examples/example.bib",
+        dois_yaml="examples/example.yaml",
+        replacements_yaml="examples/replacements.yaml",
+        static_bib="examples/not_on_crossref.bib",
+        doi2bib_database="examples/yaml2bib-doi2bib.db",
+        crossref_database="examples/yaml2bib-crossref.db",
         email="basnijholt-yaml2bib@github.com",  # fake email
     )
