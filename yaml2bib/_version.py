e78c24315c1089ac4f5f859b4c7791fe3901c20a
Bas Nijholt <bas@nijho.lt>
2023-05-01 18:02:07 -0700

    Try errors.NotVersioningitError

--- yaml2bib/_version.py
+++ yaml2bib/_version.py
@@ -3,4 +3,7 @@ from pathlib import Path
 import versioningit
 
 REPO_ROOT = Path(__file__).parent.parent
-__version__ = versioningit.get_version(project_dir=REPO_ROOT)
+try:
+    __version__ = versioningit.get_version(project_dir=REPO_ROOT)
+except versioningit.errors.NotVersioningitError:
+    __version__ = "unknown"
