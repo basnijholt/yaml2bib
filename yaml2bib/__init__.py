cb0e293743e249a1a64ca111c9cedf341ca8fd1d
Bas Nijholt <bas@nijho.lt>
2023-05-01 17:46:52 -0700

    Fix version string

--- yaml2bib/__init__.py
+++ yaml2bib/__init__.py
@@ -1,7 +1,9 @@
 """Convert a yaml file to bib."""
 
-__version__ = "0.1.4.dev0"
-
+from yaml2bib._version import __version__
 from yaml2bib._yaml2bib import yaml2bib
 
-__all__ = ["yaml2bib"]
+__all__ = [
+    "yaml2bib",
+    "__version__",
+]
